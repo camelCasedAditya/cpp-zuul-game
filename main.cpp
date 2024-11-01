8b60e857e5b8e1ca22ee1f4348775b2e680d5836
U-BSD\514957 <514957@SHS-CYBF6X3.beaverton.k12.or.us>
2024-11-01 09:41:24 -0700

    Added all the room exits

--- main.cpp
+++ main.cpp
@@ -43,15 +43,88 @@ int main() {
   cHall->setDescription(temp);
   
   Room* boringClassRoom = new Room();
+  strcpy(temp, "You are in a very boring classroom");
+  boringClassRoom->setDescription(temp);
   
   Room* dHall = new Room();
+  strcpy(temp, "You are in d hallway");
+  dHall->setDescription(temp);
+  
   Room* mainOffice = new Room();
+  strcpy(temp, "You are in the school main office");
+  mainOffice->setDescription(temp);
+  
   Room* computerLab = new Room();
+  strcpy(temp, "You are in the computer lab");
+  computerLab->setDescription(temp);
+  
   Room* tWing = new Room();
+  strcpy(temp, "You are in the t-building");
+  tWing->setDescription(temp);
+  
   Room* artRoom = new Room();
+  strcpy(temp, "You are in the art room");
+  artRoom->setDescription(temp);
+  
   Room* gym = new Room();
+  strcpy(temp, "You are in the gym");
+  gym->setDescription(temp);
+  
   Room* spanishClass = new Room();
+  strcpy(temp, "You are in the spanish classroom");
+  spanishClass->setDescription(temp);
 
+
+  start->setExit('n', cppClass);
+  start->setExit('s', oneHall);
+  start->setExit('e', courtYard);
+  start->setExit('w', tWing);
+
+  cppClass->setExit('n', spanishClass);
+  cppClass->setExit('s', start);
+
+  spanishClass->setExit('s', cppClass);
+
+  tWing->setExit('w', start);
+  tWing->setExit('e', artRoom);
+  
+  artRoom->setExit('w', tWing);
+  artRoom->setExit('s', gym);
+
+  gym->setExit('n', artRoom);
+
+  courtYard->setExit('e', start);
+  courtYard->setExit('w', mainOffice);
+
+  mainOffice->setExit('n', computerLab);
+  mainOffice->setExit('s', dHall);
+  mainOffice->setExit('e', courtYard);
+
+  computerLab->setExit('s', mainOffice);
+
+  dHall->setExit('n', mainOffice);
+  dHall->setExit('s', cHall);
+
+  cHall->setExit('n', dHall);
+  cHall->setExit('s', englishClass);
+  cHall->setExit('w', boringClassRoom);
+
+  boringClassRoom->setExit('e', cHall);
+
+  englishClass->setExit('n', cHall);
+  englishClass->setExit('e', aHall);
+
+  aHall->setExit('w', englishClass);
+  aHall->setExit('e', aWing);
+
+  aWing->setExit('n', library);
+  aWing->setExit('w', aHall);
+
+  library->setExit('n', oneHall);
+  library->setExit('s', aWing);
+
+  oneHall->setExit('n', start);
+  oneHall->setExit('s', library);
   
   bool hasItems = false;
   vector<Item*> inventory;
